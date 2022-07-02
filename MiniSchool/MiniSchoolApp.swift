fc0a43b82dc255478e6104dc10280d5be70ed7bb
이석호 <mercen@mercen.net>
2022-07-02 17:43:44 +0900

    Initial Commit

---
+++ MiniSchool/MiniSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSchoolApp.swift
+//  MiniSchool
+//
+//  Created by Mercen on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct MiniSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
